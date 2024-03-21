52775cf5841cd5483a6d41cdc79f1c1ebc6343fa
jonnvega <47796239+jonnvega@users.noreply.github.com>
2024-03-21 16:39:41 -0600

    fix bg colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     <main className="text-center mx-auto px-2 max-w-3xl">
       <h1 className="text-3xl font-semibold my-8">Welcome to Whisp</h1>
       <section className="my-8">
-        <p className="text-lg text-gray-700 dark:text-gray-400">
+        <p className="text-lg text-gray-400">
           Welcome to WHISP, a Geo Spatial analysis tool designed to aid in zero-deforestation regulation claims.
           Utilize our platform to analyze your plots and obtain data from a variety of layers. Geometries can be provided
           through Geoids from AgStack as well as Well-Known-Text (WKT) as well as with the Geojson standard.
@@ -19,13 +19,13 @@ export default function Home() {
       </section>
       <h1 className="text-3xl font-semibold my-6">Choose your Format</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-10 my-10 mx-4">
-        <Link href="/submit-geo-ids" onClick={() => resetStore()} className="block p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          <h5 className="mb-2 text-1xl font-bold tracking-tight text-gray-900 dark:text-white">Upload Geo IDs</h5>
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">If you have a Geo ID from the Asset Registry, you can get an analysis that way.</p>
+        <Link href="/submit-geo-ids" onClick={() => resetStore()} className="block p-6 border rounded-lg shadow bg-gray-800 border-gray-700">
+          <h5 className="mb-2 text-1xl font-bold tracking-tight text-white">Upload Geo IDs</h5>
+          <p className="mb-3 font-normal text-gray-400">If you have a Geo ID from the Asset Registry, you can get an analysis that way.</p>
         </Link>
-        <Link href="/submit-geometry" onClick={() => resetStore()} className="block p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          <h5 className="mb-2 text-1xl font-bold tracking-tight text-gray-900 dark:text-white">Send Geometry</h5>
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Submit your WKT or Geojson and get your plots analyzed.</p>
+        <Link href="/submit-geometry" onClick={() => resetStore()} className="block p-6 border  rounded-lg shadow bg-gray-800 border-gray-700">
+          <h5 className="mb-2 text-1xl font-bold tracking-tight text-white">Send Geometry</h5>
+          <p className="mb-3 font-normal text-gray-400">Submit your WKT or Geojson and get your plots analyzed.</p>
         </Link>
       </div>
     </main>
