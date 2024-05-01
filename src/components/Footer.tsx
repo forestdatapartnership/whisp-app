1bd682bb814dde14aa5be5b92c956bc55de838bd
jonnvega <jonn.vega@gmail.com>
2024-05-01 22:02:55 +0200

    update layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,14 @@
+
 import Image from 'next/image';
 import Link from 'next/link';
 
 const Footer: React.FC = () => {
     return (
-        <footer className="bg-transparent text-white mt-4 pt-4 border-t border-gray-700">
+        <footer className="bg-transparent text-white mt-4 py-4 border-t border-gray-700 h-16 flex items-center">
             <div className="w-full flex justify-between items-center px-4 md:px-12">
                 <div>
-                    <Link href="https://github.com/your-username/your-repository" passHref>
+                    <Link href="https://github.com/forestdatapartnership/whisp-app" passHref>
                         <Image src="/github-logo.svg" alt="GitHub Logo" width={30} height={30} />
-                          <span>View on GitHub </span>
                     </Link>
                 </div>
                 <div>
