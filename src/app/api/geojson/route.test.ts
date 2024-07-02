a2a0add579835260262f7db7cefc84fa899289d4
Jonathan V <jonn.vega@gmail.com>
2024-07-02 20:45:44 +0200

    add more tests and add npm run test to dockerfile

--- src/app/api/geojson/route.test.ts
+++ src/app/api/geojson/route.test.ts
@@ -26,7 +26,8 @@ describe('POST /api/geojson', () => {
     }
   });
 
-  it('returns a successful analysis response', async () => {
+
+  it('returns a successful analysis response for a point geometry', async () => {
     const body = {
       type: "Point",
       coordinates: [
@@ -44,27 +45,70 @@ describe('POST /api/geojson', () => {
     // Add more expectations based on the response structure
   });
 
-  // it('returns 400 for invalid input', async () => {
-  //   const body = {
-  //     // Invalid geoJSON data
-  //   };
-  //
-  //   const res = await request(server)
-  //     .post('/api/geojson')
-  //     .send(body)
-  //     .set('Accept', 'application/json');
-  //
-  //   expect(res.statusCode).toBe(400);
-  //   expect(res.body).toHaveProperty('error');
-  // });
-  //
-  // it('returns 500 for missing body', async () => {
-  //   const res = await request(server)
-  //     .post('/api/geojson')
-  //     .send({})
-  //     .set('Accept', 'application/json');
-  //
-  //   expect(res.statusCode).toBe(500);
-  //   expect(res.body).toHaveProperty('error');
-  // });
+  it('returns a successful analysis response, for a multipoint', async () => {
+    const body =
+    {
+      type: "MultiPoint",
+      coordinates: [
+        [
+          10,
+          11.2
+        ],
+        [
+          10.5,
+          11.9
+        ]
+      ]
+    }
+    const res = await request(server)
+      .post('/api/geojson')
+      .send(body)
+      .set('Accept', 'application/json');
+
+    expect(res.statusCode).toBe(200);
+    // Add more expectations based on the response structure
+  });
+
+  it('returns a successful analysis response for a set of geoids', async () => {
+    const body =
+    {
+      geoIds: [
+        "c73172da7a0a50c87eed2a08c3acd65f8c8dd1033e84f1fcbe6ce2b90701ce0d",
+        "2ff3962841cdfe820b31e03c54b774b724afbc5dafbf742bd0c8a0fc3a664a36",
+        "88bec54ad04804f5b1fafbc131266640a129be2840fa6797cda358d7e831b907",
+        "c288d6c94efa9011c0e3452af9f7fa0941661377030e10d29c68764617f9816d",
+        "1a41a309ae2387f36a604c9a6c81887e64357a7f61d228758e23ef766286fcd7",
+        "1a4472dc40700ef33f931863f58d444f243d64418616678fcf85c57e1f4bbf45",
+        "b84f55de2b7f3c77d1cbeb8b026a1b29be42d8b08d92058c9143e0556456820f",
+        "0520cfac98fbc1bd7952b1c07a9f6983b83625722b6f665ea83ac9aad3512918",
+        "b7c15efb6e3c63fcfe649a2d994973a6f5caa844f720f0edb7cf24f6a6c3c1b3"
+      ]
+    }
+
+    const res = await request(server)
+      .post('/api/geo-ids')
+      .send(body)
+      .set('Accept', 'application/json');
+
+    expect(res.statusCode).toBe(200);
+    // Add more expectations based on the response structure
+  });
+
+  it('returns a successful analysis response', async () => {
+    const body = {
+      type: "Point",
+      coordinates: [
+        10,
+        11.2
+      ]  // Add the actual geoJSON data you want to test with
+    };
+
+    const res = await request(server)
+      .post('/api/geojson')
+      .send(body)
+      .set('Accept', 'application/json');
+
+    expect(res.statusCode).toBe(200);
+    // Add more expectations based on the response structure
+  });
 });
