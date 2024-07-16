b99b3ba158901070d8b1c3fd69c84a6ad4c4a491
Jonathan V <jonn.vega@gmail.com>
2024-07-16 14:10:28 +0200

    update logo and favicon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,24 @@
 // components/Navbar.tsx
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Navbar: React.FC = () => {
     return (
         <nav className="bg-gray-800 text-white p-4 mb-4">
             <div className="w-full flex justify-between">
-                <div className="flex md:mx-12 justify-start">
-                    <Link href="/" className="text-lg hover:text-gray-300">
-                        <strong className="font-bold">W</strong>hat <strong className="font-bold">is</strong> in that <strong className="font-bold">p</strong>lot?  <strong className="font-bold">(WHISP)</strong>
+                <div className="flex md:mx-12 justify-center items-center h-full">
+                    <Link href="/" className="text-lg hover:text-gray-300 flex items-center">
+                        <Image
+                            src="whisp_logo.svg"
+                            alt="Picture of the author"
+                            width={35}
+                            height={35}
+                        />
+                        <strong className="font-bold ml-2">WHISP</strong>
                     </Link>
                 </div>
-                <div className="flex mx-12 justify-end">
+
+                <div className="flex mx-12 justify-end items-center ">
                     <Link target="_blank" href="https://openforis.org/solutions/whisp" className="hover:text-gray-300 mx-4">
                         About
                     </Link>
