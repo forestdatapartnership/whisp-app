28db2540128757577777b654c13f4e81fae879eb
Jonathan V <jonn.vega@gmail.com>
2024-07-17 19:38:24 +0200

    close file to avoid leak

--- src/utils/analizePlots.ts
+++ src/utils/analizePlots.ts
@@ -9,6 +9,7 @@ export const analyzePlots = async (payload: any) => {
     const token = uuidv4();
     const filePath = path.join(process.cwd(), 'temp');
 
+    let fileHandle;
     try {
         // Write the payload to a file
         await fs.writeFile(`${filePath}/${token}.json`, JSON.stringify(payload));
@@ -17,10 +18,12 @@ export const analyzePlots = async (payload: any) => {
 
         // Attempt to analyze the plots
         const analyzed = await analyze(token);
+ 
         if (analyzed) {
             // Read and parse the analysis results
-            const data = await fs.readFile(`${filePath}/${token}-result.json`, 'utf8');
-            const jsonData = JSON.parse(data);
+            fileHandle = await fs.open(`${filePath}/${token}-result.json`, 'r'); // Explicitly open the file
+            const fileContents = await fileHandle.readFile('utf8'); 
+             const jsonData = JSON.parse(fileContents);
             return NextResponse.json(
                 { data: jsonData, token: token }
             )
@@ -31,5 +34,10 @@ export const analyzePlots = async (payload: any) => {
     } catch (error) {
         console.error(`Error processing analysis for ${token}:`, error);
         throw error;
+    } finally {
+        // Explicitly close the file handle if it was opened
+        if (fileHandle) {
+            fileHandle.close();
+        }
     }
 }
