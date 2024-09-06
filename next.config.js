58b3322afb8eabfb81a32dbb9e70da049ed63d88
jonnvega <jonn.vega@gmail.com>
2024-09-06 15:36:32 +0200

    Add cors headers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+nextConfig = {
+    async headers() {
+      return [
+        {
+          // Apply these headers to all API routes
+          source: '/api/:path*',
+          headers: [
+            { key: 'Access-Control-Allow-Origin', value: '*' },
+            { key: 'Access-Control-Allow-Methods', value: 'GET, POST, PUT, DELETE, OPTIONS' },
+            { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
+          ],
+        },
+      ];
+    },
+};
 
 module.exports = nextConfig
