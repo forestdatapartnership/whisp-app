aee70a9997f42f8c3d13789096b595d18d8518db
radu urdea <radu.urdea@hotmail.com>
2024-09-26 16:03:02 +0300

    refactored middlewares;

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,39 +1,4 @@
-import { NextResponse } from 'next/server';
-import { NextRequest } from 'next/server';
+import { stackMiddlewares } from "./middlewares/stackMiddlewares";
+import { withCors } from "./middlewares/withCors";
 
-const corsOptions = {
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-  }
-
-export function middleware(request: NextRequest) {
-    const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') || [];
-    const origin = request.headers.get('origin') ?? '';
-    const isAllowedOrigin = allowedOrigins.includes(origin)
-    
-    const isPreflight = request.method === 'OPTIONS'
-    if (isPreflight) {
-        const preflightHeaders = {
-        ...(isAllowedOrigin && { 'Access-Control-Allow-Origin': origin }),
-        ...corsOptions,
-        }
-        return NextResponse.json({}, { headers: preflightHeaders })
-    }
-
-    // Handle other requests
-    const response = NextResponse.next()
-    
-    if (isAllowedOrigin) {
-        response.headers.set('Access-Control-Allow-Origin', origin)
-    }
-    
-    Object.entries(corsOptions).forEach(([key, value]) => {
-        response.headers.set(key, value)
-    })
-    
-    return response
-}
- 
-export const config = {
-  matcher: '/:path*',
-};
+export default stackMiddlewares([withCors]);
