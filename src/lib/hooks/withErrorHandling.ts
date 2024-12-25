0fc9d322fc4dab97dd918d47011a4592b558d67d
jonnvega <jonn.vega@gmail.com>
2024-12-25 20:05:18 +0100

    refactor compose and hooks

--- src/lib/hooks/withErrorHandling.ts
+++ src/lib/hooks/withErrorHandling.ts
@@ -5,9 +5,10 @@ import { LogFunction } from "@/lib/logger";
 export function withErrorHandling(
   handler: (req: NextRequest, log: LogFunction, ...args: any[]) => Promise<NextResponse>
 ) {
-  return async function(req: NextRequest, log: LogFunction, ...args: any[]): Promise<NextResponse> {
+  return async function(req: NextRequest, ...args: any[]): Promise<NextResponse> {
+    const [log, ...rest] = args;
     try {
-      return await handler(req, log, ...args);
+      return await handler(req, log, ...rest);
     } catch (error: any) {
       log('error', 'Error occurred: ' + error);
       return useBadRequestResponse("Error in analysis. Please check your input.");
