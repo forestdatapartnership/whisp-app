1c8e849b8e498ebd952b15c7fb999b5ac064210c
Jonas Spekker <jospek.dev@gmail.com>
2025-02-07 15:15:34 +0100

    Update page.tsx
    Updated welcome text and short explanation of GeoJSON requirements to prevent output errors.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,12 +12,34 @@ export default function Home() {
       <h1 className="text-3xl font-semibold mt-8">Welcome to Whisp</h1>
       <section className="mt-8">
         <p className="text-lg text-gray-400">
-          Welcome to WHISP, a Geo Spatial analysis tool designed to aid in zero-deforestation regulation claims. Upload your WKT or geojson to our API to receive a plot or point based analysis built from carefully selected layers processed via Google Earth Engine.
+          Welcome to Whisp, a geospatial analysis tool designed to support zero-deforestation regulation claims. Upload your geometries in WKT or GeoJSON format here to receive a plot- or point-based analysis from our{' '}
+          <a 
+            href="https://whisp.openforis.org/documentation/api-guide" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            className="text-[#3B82F6] hover:underline"
+          >
+            API
+          </a>, calculated from carefully selected{' '}
+          <a 
+            href="https://whisp.openforis.org/documentation/layers" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            className="text-[#3B82F6] hover:underline"
+          >
+            global and regional map datasets
+          </a>{' '} 
+          processed via Google Earth Engine.
         </p>
       </section>
       <div className='my-8'>
         <SubmitGeometry />
       </div>
+      <section className="mt-8">
+        <p className="text-lg text-gray-400 italic">
+          Make sure that the geometries are in WGS84 Coordinate Reference System (EPSG code: 4326) and that your file does not contain entries without coordinates to avoid errors.
+        </p>
+      </section>
     </main>
   );
 }
