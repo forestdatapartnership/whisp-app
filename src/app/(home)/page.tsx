ee004bd39594f7dc9dc9c771437e7bd0af8f8d52
jonnvega <jonn.vega@gmail.com>
2025-04-27 21:40:58 -0600

    add login and register options

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,24 +1,9 @@
 'use client'
 
-import { useStore } from '@/store';
 import SubmitGeometry from '@/components/SubmitGeometry';
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import { parseCookies } from 'nookies';
+import Link from 'next/link';
 
 export default function Home() {
-  const resetStore = useStore((state) => state.reset);
-  const router = useRouter();
-
-  useEffect(() => {
-    // Check if token exists in cookies
-    const cookies = parseCookies();
-    if (cookies.token) {
-      // Redirect to dashboard if authenticated
-      router.push('/dashboard');
-    }
-  }, [router]);
-
   return (
     <main className="text-center mx-auto px-2 max-w-3xl">
       <h1 className="text-3xl font-semibold mt-8">Welcome to Whisp</h1>
@@ -44,25 +29,29 @@ export default function Home() {
           processed via Google Earth Engine.
         </p>
       </section>
+
       <section className="mt-8 text-center">
-        <h2 className="text-xl font-semibold mb-4">Need an API key?</h2>
+        <h2 className="text-xl font-semibold mb-4">Need a permanent API key?</h2>
         <div className="flex justify-center gap-4">
-          <button
-            onClick={() => window.location.href = '/login'}
-            className="px-6 py-3 bg-blue-500 text-white font-bold rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
-          >
-            Login
-          </button>
-          <button
-            onClick={() => window.location.href = '/register'}
-            className="px-6 py-3 bg-green-500 text-white font-bold rounded hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500"
-          >
-            Register
-          </button>
+          <Link href="/login">
+            <button
+              className="px-6 py-3 bg-blue-500 text-white font-bold rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            >
+              Login
+            </button>
+          </Link>
+          <Link href="/register">
+            <button
+              className="px-6 py-3 bg-green-500 text-white font-bold rounded hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500"
+            >
+              Register
+            </button>
+          </Link>
         </div>
       </section>
+      
       <div className='my-8'>
-        <SubmitGeometry />
+        <SubmitGeometry usePublicEndpoints={true} />
       </div>
 
       <section className="mt-8">
