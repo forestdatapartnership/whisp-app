aae1afd07317f11cf28b0f53b8f3b6fb61cd03b0
jonnvega <jonn.vega@gmail.com>
2025-05-07 12:10:26 -0600

    align text left in alert

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -57,28 +57,28 @@ const Alert: React.FC<AlertProps> = ({ type, message, onClose }) => {
   return (
     <div className={`mb-6 p-3 ${bgColor} border ${borderColor} rounded-lg flex items-center gap-2`}>
       <svg 
-        xmlns="http://www.w3.org/2000/svg" 
-        className={`h-5 w-5 ${textColor} flex-shrink-0`} 
-        viewBox="0 0 20 20" 
-        fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg" 
+      className={`h-5 w-5 ${textColor} flex-shrink-0`} 
+      viewBox="0 0 20 20" 
+      fill="currentColor"
       >
-        {iconPath}
+      {iconPath}
       </svg>
-      <span className={`text-sm ${textColor}`}>{message}</span>
+      <span className={`text-sm ${textColor} text-left`}>{message}</span>
       {onClose && (
-        <button 
-          onClick={onClose} 
-          className="ml-auto text-gray-400 hover:text-gray-200"
-          aria-label="Close"
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
-            <path 
-              fillRule="evenodd" 
-              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" 
-              clipRule="evenodd" 
-            />
-          </svg>
-        </button>
+      <button 
+        onClick={onClose} 
+        className="ml-auto text-gray-400 hover:text-gray-200"
+        aria-label="Close"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+        <path 
+          fillRule="evenodd" 
+          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" 
+          clipRule="evenodd" 
+        />
+        </svg>
+      </button>
       )}
     </div>
   );
