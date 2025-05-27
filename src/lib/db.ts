d04674cc15c45ba32b412134f1ef5a536695acb8
jonnvega <jonn.vega@gmail.com>
2025-05-27 20:24:03 +0000

    update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,22 +4,8 @@ import { SecretManagerServiceClient } from '@google-cloud/secret-manager';
 
 let pool: Pool | null = null;
 
-async function getDbPassword(): Promise<string | undefined> {
-  if (process.env.DB_PASSWORD) return process.env.DB_PASSWORD;
-  if (!process.env.GCP_PROJECT_ID || !process.env.SECRET_NAME) return;
-
-  const client = new SecretManagerServiceClient();
-  const [version] = await client.accessSecretVersion({
-    name: `projects/${process.env.GCP_PROJECT_ID}/secrets/${process.env.SECRET_NAME}/versions/latest`,
-  });
-
-  return version.payload?.data?.toString();
-}
-
 export async function getPool(): Promise<Pool> {
   if (pool) return pool;
-
-  const password = await getDbPassword();
 
   if (process.env.DB_CONNECTION_TYPE === 'cloudsql') {
     const connector = new Connector();
@@ -32,7 +18,7 @@ export async function getPool(): Promise<Pool> {
       user: process.env.DB_USER,
       host: process.env.DB_HOST,
       database: process.env.DB_NAME,
-      password,
+      password: process.env.DB_PASSWORD,
     });
   } else {
     pool = new Pool({
