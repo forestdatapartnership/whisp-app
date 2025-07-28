7179155eaaa7a7d919d67de4b3aac5c05b8e9434
radu urdea <radu.urdea@totalsoft.ro>
2025-07-28 12:32:51 -0500

    removed deprecated sql scripts;

--- src/db/dev_data.sql
+++
@@ -1,4 +0,0 @@
--- Ensure temp user exists
-INSERT INTO users (id, name, last_name, email, password_hash, email_verified)
-VALUES (-1000, 'Temp', 'User', 'temp@temp.com', crypt('strongPassword123', gen_salt('bf')), TRUE)
-ON CONFLICT (id) DO NOTHING;
