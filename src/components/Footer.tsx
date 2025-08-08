bbb30edcaa882407c8b312357cbb4f2ccc53afa4
radu urdea <radu.urdea@totalsoft.ro>
2025-08-08 16:25:19 -0600

    chore: add MIT license file and update package.json with license information; updated footer links;

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,14 +9,14 @@ const Footer: React.FC = () => {
         <footer className="bg-transparent text-white mt-4 py-4 border-t border-gray-700 h-16 flex items-center">
             <div className="w-full flex justify-between items-center px-4 md:px-12">
                 <div>
-                    <Link href="https://github.com/forestdatapartnership/whisp-app" passHref>
+                    <Link href="https://github.com/forestdatapartnership/whisp-app" passHref target="_blank">
                         <Image src="/github-logo.svg" alt="GitHub Logo" width={30} height={30} />
                     </Link>
                 </div>
                 <div className="text-sm">
                     © {new Date().getFullYear()}
                     <Link href="https://openforis.org" target="_blank" className="text-blue-500"> Open Foris </Link>
-                    | <Link href="https://choosealicense.com/licenses/mit/" target="_blank" className="text-blue-500"> MIT License </Link>
+                    | <Link href="https://github.com/forestdatapartnership/whisp-app/blob/HEAD/LICENSE" target="_blank" className="text-blue-500"> MIT License </Link>
                     | <Link href={`https://github.com/forestdatapartnership/whisp-app/releases`} target="_blank" className="text-blue-500">v{version}</Link> · <WhatsNewLink version={version} />
                 </div>
             </div>
